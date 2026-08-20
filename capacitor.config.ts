4a3c853ad690eee382900ec069b97850bb7c8722
johnpaulpatigas <johnpaulpatigas@gmail.com>
2026-08-21 01:39:56 +0800

    feat: configure Capacitor and add Android platform support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.johnpaulpatigas.letreviewer',
+  appName: 'LET Reviewer',
+  webDir: 'dist'
+};
+
+export default config;
